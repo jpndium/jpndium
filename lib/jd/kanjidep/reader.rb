c3d4e88b8f72929b8cc87cc3772d362ab6cb988a
mrpudn <mrpudn@gmail.com>
2025-04-07 00:59:32 -0400

    Add kanjidep reader

---
+++ lib/jd/kanjidep/reader.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require "json"
+require_relative "../jsonl_reader"
+require_relative "dependency_resolver"
+
+module JD
+  module Kanjidep
+    # Reads chiseids jsonl files and derives dependency information for each
+    # Ideographic Description Sequence (IDS).
+    class Reader
+      PREFIXES = <<~CHARACTERS.split.freeze
+        ⿰ ⿱ ⿲ ⿳ ⿴ ⿵ ⿶ ⿷ ⿼ ⿸ ⿹ ⿺ ⿽ ⿻ ⿾ ⿿
+      CHARACTERS
+      JOINABLE_FIELDS = %i[pattern composition dependencies dependents].freeze
+
+      def from_chiseids(path)
+        chiseids = jsonl_reader.read_file(path)
+        characters = read_characters(chiseids)
+        resolver = JD::Kanjidep::DependencyResolver.resolve(characters)
+        characters.each do |character|
+          add_dependency_fields(character, resolver)
+          update_joinable_fields(character)
+        end
+      end
+
+      private
+
+      def read_characters(chiseids)
+        chiseids.map do |row|
+          pattern = row["ids"].chars
+            .select { |character| PREFIXES.include?(character) }
+          composition = split_ids(clean_ids(row["ids"]))
+            .reject { |character| character == row["character"] }
+          {
+            character: row["character"],
+            pattern: pattern,
+            composition: composition
+          }
+        end
+      end
+
+      def clean_ids(ids)
+        ids
+          .then { |i| keep_only_first_sequence(i) }
+          .then { |i| remove_text_in_brackets(i) }
+          .then { |i| remove_spaces(i) }
+          .then { |i| remove_prefixes(i) }
+      end
+
+      def keep_only_first_sequence(ids)
+        ids.gsub(%r{/.*}, "")
+      end
+
+      def remove_text_in_brackets(ids)
+        ids.gsub(/\[[^\]]*\]/, "")
+      end
+
+      def remove_spaces(ids)
+        ids.gsub(/\s/, "")
+      end
+
+      def remove_prefixes(ids)
+        ids
+          .chars
+          .reject { |character| PREFIXES.include?(character) }
+          .join
+      end
+
+      def split_ids(ids)
+        ids
+          .then { |i| split_on_codepoints(i) }
+          .map do |part|
+            next [part] if part.start_with?("&")
+
+            part.chars
+          end
+          .reduce(:+)
+      end
+
+      def split_on_codepoints(ids)
+        ids.split(/(&[^;]*;)/)
+      end
+
+      def add_dependency_fields(character, resolver)
+        dependencies = resolver.fetch_dependencies(character[:character])
+        character[:dependencies] = dependencies
+
+        dependents = resolver.fetch_dependents(character[:character])
+        character[:dependents] = dependents
+      end
+
+      def update_joinable_fields(character)
+        JOINABLE_FIELDS.each do |field|
+          character[field] = character[field].join(" ")
+        end
+      end
+
+      def jsonl_reader
+        @jsonl_reader ||= JD::JsonlReader.new
+      end
+    end
+  end
+end
---
+++ spec/jd/kanjidep/reader_spec.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+require "jd/kanjidep/reader"
+
+RSpec.describe JD::Kanjidep::Reader do
+  path = "chiseids.jsonl"
+
+  let(:reader) { described_class.new }
+  let(:chiseids) do
+    [{ character: "A", ids: "⿰火水⿱土風" }]
+  end
+  let(:lines) do
+    chiseids.map { |row| JSON.dump(row) }
+  end
+  let(:resolver) do
+    resolver = double
+    allow(resolver)
+      .to receive_messages(
+        resolve: nil,
+        fetch_dependencies: ["dependencies"],
+        fetch_dependents: ["dependents"]
+      )
+    resolver
+  end
+  let(:kanjidep) do
+    reader
+      .from_chiseids(path)
+      .each_with_object({}) do |character, hash|
+        hash[character[:character]] = character
+      end
+  end
+
+  before do
+    allow(File).to receive(:open).and_call_original
+    allow(File).to receive(:open).with(path).and_yield(lines)
+    allow(JD::Kanjidep::DependencyResolver)
+      .to receive(:resolve)
+      .and_return(resolver)
+  end
+
+  describe "#from_chiseids" do
+    let(:chiseids) do
+      [{ character: "A", ids: "⿰火水⿱土風" }]
+    end
+
+    it "sets the pattern to the prefix characters from the IDS" do
+      expect(kanjidep["A"][:pattern]).to eq("⿰ ⿱")
+    end
+
+    context "when the IDS has no prefix characters" do
+      let(:chiseids) do
+        [{ character: "A", ids: "火水" }]
+      end
+
+      it "sets the pattern to an empty array" do
+        expect(kanjidep["A"][:pattern]).to eq("")
+      end
+    end
+
+    context "when the IDS has a codepoint between & and ;" do
+      let(:chiseids) do
+        [{ character: "A", ids: "⿰火&ABC-123A;⿱&DEF-456B;風" }]
+      end
+
+      it "reads the codepoint as one character" do
+        expect(kanjidep["A"][:composition])
+          .to eq("火 &ABC-123A; &DEF-456B; 風")
+      end
+    end
+
+    context "when the IDS has text in square brackets" do
+      let(:chiseids) do
+        [
+          { character: "A", ids: "⿱鄉香[T]" },
+          { character: "B", ids: "⿱鄉香[ABC]" },
+          { character: "C", ids: "⿱[A]鄉[BC]香[DEF]" }
+        ]
+      end
+
+      it "ignores a bracketed character" do
+        expect(kanjidep["A"][:composition]).to eq("鄉 香")
+      end
+
+      it "ignores bracketed text" do
+        expect(kanjidep["B"][:composition]).to eq("鄉 香")
+      end
+
+      it "ignores multiple bracketed text instances" do
+        expect(kanjidep["C"][:composition]).to eq("鄉 香")
+      end
+    end
+
+    context "when the IDS has multiple sequences" do
+      let(:chiseids) do
+        [
+          { character: "A", ids: "⿰开龍 / ⿰幵龍" },
+          { character: "B", ids: "⿰开龍 / ⿰幵龍 / ⿰幵龍" }
+        ]
+      end
+
+      it "ignores a secondary sequence" do
+        expect(kanjidep["A"][:composition]).to eq("开 龍")
+      end
+
+      it "ignores all additional sequences" do
+        expect(kanjidep["B"][:composition]).to eq("开 龍")
+      end
+    end
+
+    context "when the IDS has spaces" do
+      let(:chiseids) do
+        [
+          { character: "A", ids: "⿱⿶⿱𦥑冖同⿱⿳一丿且分 [U]" },
+          { character: "B", ids: "⿱ ⿶⿱𦥑 冖同 ⿱⿳ 一丿且分 [U]" }
+        ]
+      end
+
+      it "ignores a space character" do
+        expect(kanjidep["A"][:composition]).to eq("𦥑 冖 同 一 丿 且 分")
+      end
+
+      it "ignores multiple space characters" do
+        expect(kanjidep["B"][:composition]).to eq("𦥑 冖 同 一 丿 且 分")
+      end
+    end
+
+    it "adds dependencies to each character" do
+      expect(kanjidep["A"][:dependencies]).to eq("dependencies")
+    end
+
+    it "adds dependents to each character" do
+      expect(kanjidep["A"][:dependents]).to eq("dependents")
+    end
+  end
+end
