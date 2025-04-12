4efbeca0bc811b0c8ed11da159ef9eec74f5bea4
mrpudn <mrpudn@gmail.com>
2025-04-12 17:56:35 -0400

    Add kanjidicdep reader

---
+++ lib/jd/kanjidicdep/reader.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module JD
+  module Kanjidicdep
+    # Reads rows from kanjidep and removes kanji not present in kanjidic.
+    class Reader
+      def initialize(kanjidic, kanjidep)
+        @kanjidic = kanjidic
+        @kanjidic_kanji = kanjidic.to_set { |row| row["literal"] }
+        @kanjidep = kanjidep
+      end
+
+      def self.read(kanjidic, kanjidep)
+        new(kanjidic, kanjidep).read
+      end
+
+      def read
+        @kanjidep
+          .select { |row| @kanjidic_kanji.member?(row["character"]) }
+          .map { |row| read_row(row) }
+      end
+
+      private
+
+      def read_row(row)
+        {
+          character: row["character"],
+          pattern: row["pattern"],
+          composition: keep_kanjidic_kanji(row["composition"]),
+          dependencies: keep_kanjidic_kanji(row["dependencies"]),
+          dependents: keep_kanjidic_kanji(row["dependents"])
+        }
+      end
+
+      def keep_kanjidic_kanji(text)
+        text
+          .to_s
+          .split
+          .select { |character| kanjidic_kanji?(character) }
+          .join(" ")
+      end
+
+      def kanjidic_kanji?(value)
+        @kanjidic_kanji.member?(value)
+      end
+    end
+  end
+end
---
+++ spec/jd/kanjidicdep/reader_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "jd/kanjidicdep/reader"
+
+RSpec.describe JD::Kanjidicdep::Reader do
+  let(:kanjidic_kanji) { %w[A B D E G] }
+  let(:kanjidic) do
+    kanjidic_kanji.map do |kanji|
+      { "literal" => kanji }
+    end
+  end
+  let(:kanjidep) do
+    [
+      {
+        "character" => "A",
+        "pattern" => "(pattern)",
+        "composition" => "B C",
+        "dependencies" => "B C D",
+        "dependents" => "E F"
+      },
+      {
+        "character" => "C",
+        "pattern" => "(pattern)",
+        "composition" => nil,
+        "dependencies" => nil,
+        "dependents" => nil
+      }
+    ]
+  end
+  let(:kanjidicdep) { described_class.read(kanjidic, kanjidep) }
+  let(:expected) do
+    [
+      {
+        character: "A",
+        pattern: "(pattern)",
+        composition: "B",
+        dependencies: "B D",
+        dependents: "E"
+      }
+    ]
+  end
+
+  describe "#read" do
+    it "keeps kanji from kanjidic" do
+      expect(kanjidicdep).to match_array(expected)
+    end
+  end
+end
