dde1cefd7ba9bb7fdc2743a7c8906e5e6182ab76
mrpudn <mrpudn@gmail.com>
2025-04-19 20:57:10 -0400

    Extract priority filter module from jmdictpri reader

--- lib/jd/jmdictpri/reader.rb
+++ lib/jd/jmdictpri/reader.rb
@@ -1,30 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "../jmdict/reader"
+require_relative "../jmdictpri/priority_filter"
 
 module JD
   module Jmdictpri
     # Reads priority entry elements from a JMdict XML file.
     class Reader < JD::Jmdict::Reader
-      protected
-
-      def read_each(stream)
-        super { |entry| yield entry if priority_entry?(entry) }
-      end
-
-      def priority_entry?(entry)
-        any_ke_pri?(entry) || any_re_pri?(entry)
-      end
-
-      def any_ke_pri?(entry)
-        entry[:k_ele]&.each { |el| return true if el.key?(:ke_pri) }
-        false
-      end
-
-      def any_re_pri?(entry)
-        entry[:r_ele].each { |el| return true if el.key?(:re_pri) }
-        false
-      end
+      include JD::Jmdictpri::PriorityFilter
     end
   end
 end
