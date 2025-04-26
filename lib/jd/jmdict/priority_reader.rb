fff2d294c024ae962a62abf922737ef2080aac6a
mrpudn <mrpudn@gmail.com>
2025-04-25 21:39:22 -0400

    Remove priority filter

--- lib/jd/jmdict/priority_reader.rb
+++ lib/jd/jmdict/priority_reader.rb
@@ -4,7 +4,25 @@ module JD
   module Jmdict
     # Reads priority entry elements from jmdict jsonl files.
     class PriorityReader < JD::JsonlDirectoryReader
-      include JD::Jmdictpri::PriorityFilter
+      protected
+
+      def read_file(path)
+        super { |entry| yield entry if priority_entry?(entry) }
+      end
+
+      def priority_entry?(entry)
+        any_ke_pri?(entry) || any_re_pri?(entry)
+      end
+
+      def any_ke_pri?(entry)
+        entry["k_ele"]&.each { |el| return true if el.key?("ke_pri") }
+        false
+      end
+
+      def any_re_pri?(entry)
+        entry["r_ele"]&.each { |el| return true if el.key?("re_pri") }
+        false
+      end
     end
   end
 end
