8af235b35b7dea8e1bcec783504811f15a674c1a
mrpudn <mrpudn@gmail.com>
2025-05-13 22:14:11 -0400

    Reorganize lib/jpndium.rb

--- lib/jpndium.rb
+++ lib/jpndium.rb
@@ -3,23 +3,31 @@
 require "json"
 require "open3"
 require "nokogiri"
+
 require_relative "jpndium/file_reader"
 require_relative "jpndium/file_writer"
 require_relative "jpndium/directory_reader"
 require_relative "jpndium/directory_writer"
+
 require_relative "jpndium/jsonl_reader"
 require_relative "jpndium/jsonl_writer"
 require_relative "jpndium/jsonl_directory_reader"
 require_relative "jpndium/jsonl_directory_writer"
-require_relative "jpndium/tokenizer"
+
 require_relative "jpndium/xml_helpers"
 require_relative "jpndium/xml_reader"
+
+require_relative "jpndium/tokenizer"
+
 require_relative "jpndium/chiseids/reader"
 require_relative "jpndium/chiseids/dependency_reader"
 require_relative "jpndium/chiseids/dependency_resolver"
+
 require_relative "jpndium/jmdict/reader"
 require_relative "jpndium/jmdict/priority_reader"
+
 require_relative "jpndium/jmnedict/reader"
 require_relative "jpndium/jmnedict/priority_reader"
+
 require_relative "jpndium/kanjidic/reader"
 require_relative "jpndium/kanjidic/dependency_reader"
